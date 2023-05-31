ac5735d2a3aade4b34ff916875bab947497746c1
lclong1703 <lclong1703@gmail.com>
2023-05-31 22:07:02 +0700

    OpenAPI

--- src/components/users/user.controller.ts
+++ src/components/users/user.controller.ts
@@ -1,9 +1,8 @@
-import { Controller, Post, Body, Req, Patch } from '@nestjs/common';
+import { Controller, Post, Body, Patch, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User as UserDto } from './dto/user.dto';
 import { Login } from './dto/login.dto';
 import { Verification } from './dto/verification.dto';
-import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
@@ -24,9 +23,9 @@ export class UserController {
     return this.userService.verify(verification);
   }
 
-  @Patch('set-password')
-  setPassword(@Req() request: Request, @Body() user: UserDto) {
-    const email = request.get('email');
+  @Post('set-password')
+  setPassword(@Query('emailQ') emailQ: string, @Body() user: UserDto) {
+    const email = emailQ;
     return email ? this.userService.setPassword(email, user) : '';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+import { readFileSync } from 'fs';
+import { load } from 'js-yaml';
+import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const api = load(
+    readFileSync(join(__dirname, '../openapi/openapi.yaml'), 'utf8'),
+  ) as OpenAPIObject;
+  SwaggerModule.setup('api', app, api);
   await app.listen(3000);
 }
 bootstrap();
